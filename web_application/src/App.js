534619df380bca75afcb1dc3d6c4d31f354467a9
dolevb <dolev10010@gmail.com>
2024-09-09 11:52:51 +0300

    fixed_location

--- web_application/src/App.js
+++ web_application/src/App.js
@@ -23,7 +23,6 @@ function App() {
     const checkUser = async () => {
       let user = userpool.getCurrentUser();
       if (user) {
-        console.log(user);
         setIsLoggedIn(true);
       }
     };
